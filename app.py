c3ce366c67bd368b0816d77448e52afb72d008ec
IAGO FILGUEIRAS CHIAPETTA <078463@unifeso.lan>
2025-04-21 20:24:12 -0300

    modo produção

--- app.py
+++ app.py
@@ -295,4 +295,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=7000)
+    app.run(host='0.0.0.0', port=7000)
